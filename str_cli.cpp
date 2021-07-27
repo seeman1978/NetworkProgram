20079a447849823646ad753a5bb5268a8d617cda
qiang wang <qiang.wang@megatronix.co>
2021-07-27 10:28:33 +0800

    client shutdown batch input

--- str_cli.cpp
+++ str_cli.cpp
@@ -16,7 +16,7 @@ str_cli(FILE *fp, int sockfd)
 		Fputs(recvline, stdout);
 	}
 }
-//select mplement
+//select implement
 void str_cli_select(FILE *fp, int sockfd)
 {
     int maxfdp1{0};
@@ -41,6 +41,45 @@ void str_cli_select(FILE *fp, int sockfd)
             }
 
             Writen(sockfd, sendline, strnlen(sendline, sizeof sendline));
+        }
+    }
+}
+
+//select and shutdown implement
+void str_cli_select_shutdown(FILE *fp, int sockfd)
+{
+    int maxfdp1{0}, stdineof{0};
+    fd_set rset;    //read fd sets
+    char buf[MAXLINE];
+    int n{0};
+    FD_ZERO(&rset);
+
+    for (;;){
+        if (stdineof == 0){
+            FD_SET(fileno(fp), &rset);
+        }
+        FD_SET(sockfd, &rset);
+        maxfdp1 = std::max(fileno(fp), sockfd) + 1;
+        Select(maxfdp1, &rset, nullptr, nullptr, nullptr);
+        if (FD_ISSET(sockfd, &rset)){
+            if ((n=Read(sockfd, buf, MAXLINE)) == 0){
+                if (stdineof == 1){
+                    return;
+                }
+                else{
+                    err_quit("str_cli: server terminated prematurely");
+                }
+            }
+            Write(fileno(stdout), buf, n);
+        }
+        if(FD_ISSET(fileno(fp), &rset)){
+            if ((n=Read(fileno(fp), buf, MAXLINE)) == 0){
+                stdineof = 1;
+                Shutdown(sockfd, SHUT_WR);
+                FD_CLR(fileno(fp), &rset);
+                continue;
+            }
+            Writen(sockfd, buf, n);
         }
     }
 }
--- unp.h
+++ unp.h
@@ -8,26 +8,29 @@
 #include <stdio.h>
 
 #define	MAXLINE		4096	/* max text line length */
+#define	FILE_MODE	(S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+/* default file access permissions for new files */
+#define	DIR_MODE	(FILE_MODE | S_IXUSR | S_IXGRP | S_IXOTH)
+/* default permissions for new directories */
+
 void str_cli(FILE *fp, int sockfd);
 void str_cli_select(FILE *fp, int sockfd);
+void str_cli_select_shutdown(FILE *fp, int sockfd);
 void str_echo(int sockfd);
 
+void Write(int fd, void *ptr, size_t nbytes);
 void Writen(int, void *, size_t);
+size_t Read(int fd, void *ptr, size_t nbytes);
 size_t Readline(int fd, void *ptr, size_t maxlen);
-
-void Bind(int fd, const struct sockaddr *sa, socklen_t salen);
+int Open(const char *pathname, int oflag, mode_t mode);
 
 void err_quit(const char *fmt, ...);
 void err_sys(const char *fmt, ...);
 
 void Fclose(FILE *fp);
-
 FILE* Fdopen(int fd, const char *type);
-
 char * Fgets(char *ptr, int n, FILE *stream);
-
 FILE * Fopen(const char *filename, const char *mode);
-
 void Fputs(const char *ptr, FILE *stream);
 
 //signal
@@ -36,5 +39,6 @@ Sigfunc * Signal(int signo, Sigfunc *func);
 
 //socket
 int Select(int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout);
-
+void Bind(int fd, const struct sockaddr *sa, socklen_t salen);
+void Shutdown(int fd, int how);
 #endif	/* __unp_h */
