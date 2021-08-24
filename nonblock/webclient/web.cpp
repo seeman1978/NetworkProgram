d6c8c39501c50c8438ba0cd09e14900abfe83768
qiang wang <qiang.wang@megatronix.co>
2021-08-24 10:07:55 +0800

    web client non block connect

--- nonblock/webclient/web.cpp
+++ nonblock/webclient/web.cpp
@@ -5,7 +5,8 @@
 #include <fcntl.h>
 
 int main(int argc, char** argv) {
-    int fd, n, flags,error;
+    int fd, flags, error, i;
+    socklen_t n;
     char buf[MAXLINE];
     fd_set  rs, ws;
     if (argc < 5){
@@ -14,7 +15,7 @@ int main(int argc, char** argv) {
     char *end;
     const long maxnconn = strtol(argv[1], &end, 10);
     nfiles = std::min(argc-4, MAXFILES);
-    for (int i = 0; i < nfiles; ++i) {
+    for (i = 0; i < nfiles; ++i) {
         file[i].f_name = argv[i+4];
         file[i].f_host = argv[2];
         file[i].f_flags = 0;
@@ -26,6 +27,54 @@ int main(int argc, char** argv) {
     maxfd = -1;
     nlefttoread = nlefttoconn = nfiles;
     nconn = 0;
+    while (nlefttoread > 0){
+        while (nconn < maxnconn && nlefttoconn > 0){    ///find a file to read
+            for (i = 0; i < nfiles; ++i) {
+                if (file[i].f_flags == 0){
+                    break;
+                }
+            }
+            if (i == nfiles){
+                err_quit("nlefttoconn = %d but nothing found", nlefttoconn);
+            }
+            start_connect(&file[i]);
+            ++nconn;
+            --nlefttoconn;
+        }
+        rs = rset;
+        ws = wset;
+        n = Select(maxfd+1, &rs, &ws, nullptr, nullptr);
+        for (i = 0; i < nfiles; ++i) {
+            flags = file[i].f_flags;
+            if (flags == 0 || flags&F_DONE){
+                continue;
+            }
+            fd = file[i].f_fd;
+            if (flags&F_CONNECTING && (FD_ISSET(fd, &rs) || FD_ISSET(fd, &ws))){
+                n = sizeof error;
+                if (getsockopt(fd, SOL_SOCKET, SO_ERROR, &error, &n) < 0 || error != 0){
+                    err_ret("nonblocking connect failed for %s", file[i].f_name);
+                }
+                /// connect established
+                printf("connection established for %s\n", file[i].f_name);
+                FD_CLR(fd, &wset);     /// no more writeability test
+                write_get_cmd(&file[i]);      /// write the get cmd
+            }
+            else if (flags & F_READING && FD_ISSET(fd, &rs)){
+                if ((n= Read(fd, buf, sizeof buf)) == 0){
+                    printf("end-of-file on %s\n", file[i].f_name);
+                    Close(fd);
+                    file[i].f_flags = F_DONE; /// clears F_READING
+                    FD_CLR(fd, &rset);
+                    --nconn;
+                    --nlefttoread;
+                }
+                else{
+                    printf("read %d bytes from %s\n", n, file[i].f_name);
+                }
+            }
+        }
+    }
     return 0;
 }
 
