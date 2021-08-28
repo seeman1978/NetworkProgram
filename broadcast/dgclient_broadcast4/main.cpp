0b19e0dfd142cc52e63f43def94746aebda00546
强 王 <seeman1978@163.com>
2021-08-28 23:13:56 +0800

    broadcast

---
+++ broadcast/dgclient_broadcast4/main.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include "../../unp.h"
+
+void dg_cli(FILE *fp, int sockfd, const struct sockaddr* pservaddr, socklen_t servlen){
+    int n;
+    const int on = 1;
+    char sendline[MAXLINE], recvline[MAXLINE+1];
+    fd_set rset;
+    sigset_t sigset_alrm, sigset_empty;
+    socklen_t len;
+    struct sockaddr * preply_addr;
+    preply_addr = Malloc(servlen);
+    Setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &on, sizeof(on));
+    FD_ZERO(&rset);
+
+    Sigemptyset(&sigset_empty);
+    Sigemptyset(&sigset_alrm);
+    Sigaddset(&sigset_alrm, SIGALRM);
+    Signal(SIGALRM, recvfrom_alarm);
+
+    while (Fgets(sendline, MAXLINE, fp) != nullptr){
+        Sendto(sockfd, sendline, strlen(sendline), 0, pservaddr, servlen);
+
+        Sigprocmask(SIG_BLOCK, &sigset_alrm, nullptr);
+        alarm(5);
+        for (; ; ) {
+            FD_SET(sockfd, &rset);
+            n = pselect(sockfd+1, &rset, nullptr, nullptr, nullptr, &sigset_empty);
+            if (n < 0){
+                if (neeno == EINTR){
+                    break;
+                }
+                else{
+                    err_sys("pselect error: returned %d", n);
+                }
+            }
+            else if (n != 1){
+                err_sys("pselect error: returned %d", n);
+            }
+            len = servlen;
+            n = Recvfrom(sockfd, recvline, MAXLINE, 0, preply_addr, &len);
+            recvline[n] = 0;
+            printf("from %s:%s", Sock_ntop_host(preply_addr, len), recvline);
+        }
+    }
+    free(preply_addr);
+}
+
+static void recvfrom_alarm(int signo){
+    return;
+}
+
+int main() {
+    int sockfd{0};
+    struct sockaddr_in servaddr{} ;
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_port = htons(1234);
+    Inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr);
+    sockfd = Socket(AF_INET, SOCK_DGRAM, 0);
+    dg_cli(stdin, sockfd, (sockaddr*)&servaddr, sizeof(servaddr));
+
+    return 0;
+}
