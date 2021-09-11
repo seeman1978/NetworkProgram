4109c94dce81b079c2917ea15074f9e2de8be841
wq <seeman1978@163.com>
2021-09-11 09:31:17 +0800

    traceroute

--- traceroute/main.cpp
+++ traceroute/main.cpp
@@ -3,6 +3,7 @@
 #include <netinet/in.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <cstring>
 
 struct proto proto_v4
 {
@@ -14,6 +15,74 @@ struct proto proto_v4
 struct proto proto_v6 {icmpcode_v6, recv_v6, nullptr, nullptr, nullptr, nullptr, 0,
 IPPROTO_ICMPV6, IPPROTO_IPV6, IPV6_UNICAST_HOPS};
 #endif
+
+void traceloop(){
+    int seq, code, done;
+    double rtt;
+    struct rec *rec;
+    struct timeval tvrecv;
+    recvfd = Socket(pr->sasend->sa_family, SOCK_RAW, pr->icmpproto);
+    setuid(getuid());
+#ifdef IPV6
+    if (pr->sasend->sa_family == AF_INET6 && verbose == 0){
+        struct icmp6_filter myfilt;
+        ICMP6_FILTER_SETBLOCKALL(&myfilt);
+        ICMP6_FILTER_SETPASS(ICMP6_TIME_EXCEEDED, &myfilt);
+        ICMP6_FILTER_SETPASS(ICMP6_DST_UNREACH, &myfilt);
+        setsockopt(recvfd, IPPROTO_IPV6, ICMP6_FILTER, &myfilt, sizeof(myfilt));
+    }
+#endif
+    sendfd = Socket(pr->sasend->sa_family, SOCK_DGRAM, 0);
+    pr->sabind->sa_family = pr->sasend->sa_family;
+    sport = (getpid() & 0xffff)|0x8000; ///our source udp port
+    sock_set_port(pr->sabind, pr->salen, htons(sport));
+    Bind(sendfd, pr->sabind, pr->salen);
+    sig_alrm(SIGALRM);
+
+    seq = 0;
+    done = 0;
+    for (ttl = 1; ttl <= max_ttl && done == 0; ttl++) {
+        Setsockopt(sendfd, pr->ttllevel, pr->ttloptname, &ttl, sizeof(int));
+        bzero(pr->salast, pr->salen);
+        printf("%2d ", ttl);
+        fflush(stdout);
+
+        for(probe = 0; probe < nprobes; probe++){
+            rec = (struct rec*)sendbuf;
+            rec->rec_seq = ++seq;
+            rec->rec_ttl = ttl;
+            Gettimeofday(&rec->rec_tv, nullptr);
+            sock_set_port(pr-.sasend, pr->salen, htons(dport + seq));
+            Sendto(sendfd, sendbuf, datalen, 0, pr->sasend, pr->salen);
+            if((code = (*pr->recv)(seq, &tvrecv)) == -3){
+                printf(" *");/// timeout , no reply
+            }
+            else{
+                char str[NI_MAXHOST];
+                if (sock_cmp_addr(pr->sarecv, pr->salast, pr->salen) != 0){
+                    if (getnameinfo(pr->sarecv, pr->salen, str, sizeof(str), nullptr, 0, 0) == 0){
+                        printf(" %s (%s) ", str, Sock_ntop_host(pr->sarecv, pr->salen));
+                    }
+                    else{
+                        printf(" %s", Sock_ntop_host(pr->sarecv, pr->salen));
+                    }
+                    memcpy(pr->salast, pr->sarecv, pr->salen);
+                }
+                tv_sub(&tvrecv, &rec->rec_tv);
+                rtt = tvrecv.tv_sec * 1000.0 + tvrecv.tv_usec/1000.0;
+                printf("    %3.f ms", rtt);
+                if (code == -1){
+                    done++;
+                }
+                else if(code >= 0){
+                    printf(" (ICMP %s)", (*pr->icmpcode)(code));
+                }
+            }
+            fflush(stdout);
+        }
+        printf("\n");
+    }
+}
 
 int main(int argc, char** argv) {
     datalen = sizeof(struct rec);
