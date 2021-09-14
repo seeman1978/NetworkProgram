af0fce2671dda24a9a9b42b35f8397e3ed3efc9b
强 王 <seeman1978@163.com>
2021-09-14 09:54:56 +0800

    udp check sum

--- udpchsum/udpread.cpp
+++ udpchsum/udpread.cpp
@@ -1,3 +1,5 @@
+#include <cstdint>
+
 #include "udpcksum.h"
 
 struct udpiphdr * udp_read() {
@@ -22,5 +24,35 @@ struct udpiphdr * udp_read() {
 			default:
 				err_quit("unsupported datalink (%d)", datalink);
 		}
+	}
+}
+
+struct udpiphdr* udp_check(char* ptr, int len) {
+	int hlen;
+	struct ip* ip;
+	struct udpiphdr* ui;
+	if (len < sizeof(struct ip) + sizeof(struct udphdr)) {
+		err_quit("len=%d", len);
+	}
+	ip = (struct ip*)ptr;
+	if (ip->ip_v != IPVERSION) {
+		err_quit("ip_v = %d", ip->ip_v);
+	}
+	hlen = ip->ip_hl << 2;
+	if (hlen < sizeof(struct ip)) {
+		err_quit("ip_hl=%d", ip->ip_hl);		
+	}
+	if (len < hlen+sizeof(struct udphdr)) {
+		err_quit("len=%d, hlen=%d", len, hlen);
+	}
+	if ((ip->ip_sum = in_cksum((uint16_t*)ip, hlen)) != 0) {
+		err_quit("ip check sum error");
+	}
+	if (ip->ip_p == IPPROTO_UDP) {
+		ui = (struct udpiphdr*)ip;
+		return ui;
+	}
+	else {
+		err_quit("not a udp packet");
 	}
 }
