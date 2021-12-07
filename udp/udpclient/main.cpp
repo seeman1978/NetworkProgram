c161daed770335e4d8ce071dd4b7b0ef10e02edb
qiang wang <qiang.wang@megatronix.co>
2021-12-07 13:03:19 +0800

    udp client

--- udp/udpclient/main.cpp
+++ udp/udpclient/main.cpp
@@ -26,7 +26,7 @@ sock_ntop_host(const struct sockaddr *sa, socklen_t salen)
 void dg_cli(FILE *fp, int sockfd, const struct sockaddr* pservaddr, socklen_t servlen){
     int n, maxfdp1;
     const int on = 1;
-    char sendline[1024]{"world"}, recvline[1024];
+    char recvline[1024];
 
     socklen_t len;
     struct sockaddr * preply_addr;
@@ -38,13 +38,21 @@ void dg_cli(FILE *fp, int sockfd, const struct sockaddr* pservaddr, socklen_t se
     len = servlen;
     n = recvfrom(sockfd, recvline, 1024, 0, preply_addr, &len);
     recvline[n] = 0;
-    printf("from %s:%s\n", sock_ntop_host(preply_addr, len), recvline);
+
+    printf("from %s:", sock_ntop_host(preply_addr, len));
+    for (int i = 0; i < n; ++i) {
+        uint8_t n = recvline[i];
+        int j = n;
+        std::cout << std::hex << std::showbase << j << " ";
+    }
+    std::cout << '\n';
 
     struct sockaddr_in recvaddr{};
     bzero(&recvaddr, sizeof(recvaddr));
     recvaddr.sin_family = AF_INET;
     recvaddr.sin_addr.s_addr = INADDR_BROADCAST;
-    recvaddr.sin_port = htons(1234);
+    recvaddr.sin_port = htons(13400);
+    uint8_t sendline[1024]{0x02, 0xFD, 0x00, 0x01, };   //vehicle identification request message
     sendto(sockfd, sendline, strlen(sendline), 0, (sockaddr*)&recvaddr, sizeof(recvaddr));
 
     free(preply_addr);
@@ -58,6 +66,8 @@ int main() {
     servaddr.sin_port = htons(4321);
     servaddr.sin_addr.s_addr = INADDR_ANY;
     sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    const int on = 1;
+
     bind(sockfd, (sockaddr*)&servaddr, sizeof(servaddr));
     dg_cli(stdin, sockfd, (sockaddr*)&servaddr, sizeof(servaddr));
 
